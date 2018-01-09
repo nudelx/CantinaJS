1ed50d4fdd8a1cdd38b6e71e3b8375ca28dfb5b7
Alex Nudelman <nudelx@gmail.com>
2018-01-09 20:56:59 +0200

    added config and logo

--- cantina-js/cantina.js
+++ cantina-js/cantina.js
@@ -5,6 +5,13 @@ var path = require('path')
 const CANTINA_ENV = {
   PROJECT_ROOT: process.cwd(),
   MODULE: path.join(__dirname, '/'),
-  FS: require('fs')
+  FS: require('fs'),
+  PATH: path,
+  CONFIG_FILE_NAME: 'cantina.config.js',
+  PATH_SPLIT: '__root_'
 }
-console.log(CANTINA_ENV)
+const logo = CANTINA_ENV.FS.readFileSync(CANTINA_ENV.PATH.join(__dirname, '/') + '/src/logo.js', 'utf8')
+const config = require(`${CANTINA_ENV.PROJECT_ROOT}/${CANTINA_ENV.CONFIG_FILE_NAME}`)
+// console.log(CANTINA_ENV)
+console.log(logo)
+// console.log(config)
--- cantina.config.js
+++ cantina.config.js
@@ -1,7 +1,23 @@
-export default {
+module.exports = {
 
-  mainProjectName: 'DeathStar'
-
-
-
+  projects: [
+    {
+      name: "Project One",
+      version: "1.0.0",
+      productionBranch: "master",  // default master
+      rootFolder: '_root_/Project_1'
+    },
+    {
+      name: "Project Two",
+      version: "1.0.0",
+      productionBranch: "master",
+      rootFolder: '_root_/Project_2'
+    },
+    {
+      name: "Project Tree",
+      version: "1.0.0",
+      productionBranch: "master",
+      rootFolder: '_root_/Project_2'
+    }
+  ]
 }
